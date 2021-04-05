7a1f4e5c75d0b6cd2d96269a7160be40736a1ad1
Aaron Crosman <acrosman@gmail.com>
2021-04-05 00:55:58 -0400

    Comment Fixes

--- app/interface.js
+++ app/interface.js
@@ -1,10 +1,11 @@
+// When the content of the page is ready, fire off the sample IPC message.
 document.addEventListener("DOMContentLoaded", (event) => {
-    window.api.send('sample_message', {
-        message_content: 'Stuff and Things',
-    });
+  window.api.send("sample_message", {
+    message_content: "Stuff and Things",
+  });
 });
 
-  // Org Limits Response Handler.
-window.api.receive('sample_response', (data) => {
-    alert(data.message);
+// Sample Response Handler.
+window.api.receive("sample_response", (data) => {
+  alert(data.message);
 });
